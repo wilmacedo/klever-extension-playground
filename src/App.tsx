b0d8d805cae6a4d97a00ec2d8982b5a66679e930
Wil Macedo <wil.macedo.sa@gmail.com>
2023-07-27 13:32:51 -0300

    feat: :rocket: init project setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <div className="border border-slate-300 rounded-md shadow-sm"></div>
+    </div>
+  );
+}
+
+export default App;
